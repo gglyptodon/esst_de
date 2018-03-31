b8fc7ae45f63aa7274dba65aa02a5901f208f3e7
frogsicle <a7denton@gmail.com>
2018-03-31 20:49:01 +0200

    skeleton

---
+++ esst_de/Player.gd
@@ -0,0 +1,45 @@
+extends Area2D
+
+export (int) var SPEED  # how fast the player will move (pixels/sec)
+var screensize  # size of the game window
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+
+func _ready():
+    screensize = get_viewport_rect().size
+    # Called every time the node is added to the scene.
+    # Initialization here
+
+
+func _process(delta):
+    var velocity = Vector2() # the player's movement vector
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    
+    if velocity.x != 0:
+        $AnimatedSprite.animation = "left"
+        $AnimatedSprite.flip_v = false
+        $AnimatedSprite.flip_h = velocity.x > 0	
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * SPEED
+        if velocity.x != 0:
+            $AnimatedSprite.play()
+        else:
+            $AnimatedSprite.stop()
+    else:
+        $AnimatedSprite.stop()
+    position += velocity * delta
+    position.x = clamp(position.x, 0, screensize.x)
+    position.y = clamp(position.y, 0, screensize.y)
+    # Called every frame. Delta is time since last frame.
+    # Update game logic here.
+

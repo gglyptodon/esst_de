afaf6e71b159dd02719d1cf8d14771e0f8d15a5b
gglyptodon <janina.mass@gmail.com>
2018-04-02 21:21:52 +0200

    godot tutorial with hud and basic interaction

--- esst_de/Fish.gd
+++ esst_de/Fish.gd
@@ -11,11 +11,15 @@ func _ready():
     $AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 
 
-func _on_Visibility_screen_exited():
-    queue_free()
+#func _on_Visibility_screen_exited():
+#    queue_free()
 
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free() # replace with function body
---
+++ esst_de/Main.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+export (PackedScene) var Fish
+var score
+
+func _ready():
+    randomize()
+	
+	
+
+
+func game_over():
+    print("game over")
+    $ScoreTimer.stop()
+    $FishTimer.stop()
+    $HUD.show_game_over()
+
+
+func new_game():
+    score = 0
+    print("ng")
+    $Player.start($StartPosition.position)
+    $StartTimer.start()
+    $HUD.update_score(score)
+    $HUD.show_message("Get Ready")
+
+func _on_StartTimer_timeout():
+    $FishTimer.start()
+    $ScoreTimer.start()
+
+func _on_ScoreTimer_timeout():
+    score += 1
+    $HUD.update_score(score)
+
+
+func _on_FishTimer_timeout():
+    # choose a random location on Path2D
+    $FishPath/FishSpawnLocation.set_offset(randi())
+    # create a Mob instance and add it to the scene
+    var fish = Fish.instance()
+    add_child(fish)
+    # set the mob's direction perpendicular to the path direction
+    var direction = $FishPath/FishSpawnLocation.rotation + PI/2
+    # set the mob's position to a random location
+    fish.position = $FishPath/FishSpawnLocation.position
+    # add some randomness to the direction
+    direction += rand_range(-PI/4, PI/4)
+    fish.rotation = direction
+    # choose the velocity
+    fish.set_linear_velocity(Vector2(rand_range(fish.MIN_SPEED, fish.MAX_SPEED), 0).rotated(direction))
